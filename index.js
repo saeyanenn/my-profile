c0c6b876eb631ef781767a8f440d37247115edec
ito sae <saeito20040122@gmail.com>
2022-05-04 12:18:33 +0900

    githubapiから最終編集日を取得できるようにした。

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ $(function () {
     $(this).toggleClass("flipped");
   });
 });
+
+fetch("https://api.github.com/repos/saeyanenn/my-profile/commits")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const lastCommitDate = new Date(data[0].commit.committer.date);
+    $("#last-commit-date").text(
+      `最終更新日時：${
+        lastCommitDate.getMonth() + 1
+      }月${lastCommitDate.getDate()}日`
+    );
+  });
