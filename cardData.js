5f4c28b9e14b3a968d84e2ad033818a739366177
ito sae <saeito20040122@gmail.com>
2022-05-08 23:19:47 +0900

    jsを用いてカードを動的に表示出来るようにした

---
+++ cardData.js
@@ -0,0 +1,66 @@
+const cardData = [
+  {
+    icon: "fa-basketball",
+    frontTitle: "SPORT",
+    backTitle: "バスケットボール",
+    content:
+      "小学生の頃から高校生までずっとやってきました。<br />あまり才能があるとは言えませんでしたが、楽しさと悔しさを糧に続けてきました。<br />",
+  },
+  {
+    icon: "fa-computer",
+    frontTitle: "TECH",
+    backTitle: "プログラミング",
+    content: `高校生のことから興味があり、大学生になってから本格的に学習を始めました。<br /> 大学では<a href="https://twitter.com/szpp_3776"class="content-detail-link">プログラミングサークル</a>に入り、 より技術を磨いていきます。`,
+  },
+  {
+    icon: "fa-dog",
+    frontTitle: "ANIMAL",
+    backTitle: "犬",
+    content: `小さい頃から動物と触れ合うことが好きで、
+        今は寮生活でペットは飼えないですが、いつか飼ってみたいです。<br />
+        シベリアンハスキーの毛並みと顔が好きで、モフモフしたいです。<br />`,
+  },
+  {
+    icon: "fa-book-open",
+    frontTitle: "SUBJECT",
+    backTitle: "英語",
+    content: `英語自体は得意とは言えず、最初は嫌いだったのですが、
+        学べば学ぶほど上達する感覚があり、楽しくなってきました。<br />
+        ただ、まだネイティブとの会話の経験がないので、まだまだ課題は多いです。<br />`,
+  },
+  {
+    icon: "fa-burger",
+    frontTitle: "FOOD",
+    backTitle: "アボカド",
+    content: `アボカドは栄養豊富で彩りもいいし、
+        ご飯と一緒に食べても美味しいのでおすすめです！<br />
+        今は世界的にも人気があり、供給が需要に追いついておらず、
+        価格が高騰していて、あまり食べれていません。`,
+  },
+  {
+    icon: "fa-compact-disc",
+    frontTitle: "ARTIST",
+    backTitle: `<a href="https://yorushika.com/" class="content-title-link"
+        >ヨルシカ</a>`,
+    content: `n-bunaさんの心に響く歌詞や曲調と、
+        suisさんの透き通った歌声が大好きです。<br />
+        いつかライブに行ってみたいです。<br />`,
+  },
+  {
+    icon: "fa-robot",
+    frontTitle: "ANIMATION",
+    backTitle: "EVANGERION",
+    content: `庵野監督の精神描写とキャラクターの個性、
+        世界観やハッと気付かされる伏線が大好きです。<br />
+        旧アニメ版、漫画版、新劇場版それぞれに違いがあり、
+        個人的には新劇場版のQが一番好きです。`,
+  },
+  {
+    icon: "fa-landmark",
+    frontTitle: "AMUSEMENT PARK",
+    backTitle: "ディズニーランド",
+    content: `小さい頃に何度か連れてってもらって、大好きになりました。<br />
+        乗り物が好きというよりも、世界観が好きで、ずっと歩いていたいです。<br />
+        家族としか行ったことがないので、友達といったり、一人で行ってみたりしたいです。一人で行ってみたりしたいです。<br />`,
+  },
+];
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-$(function () {
-  $(".contents-box").on("click", function () {
-    $(this).toggleClass("flipped");
-  });
-});
-
 fetch("https://api.github.com/repos/saeyanenn/my-profile/commits")
   .then(function (response) {
     return response.json();
@@ -18,11 +12,35 @@ fetch("https://api.github.com/repos/saeyanenn/my-profile/commits")
   });
 
 function addMarginTopToFooter(times) {
-  const contentsBoxHeight = $(".contents-box-height").height();
+  const contentsBoxHeight = $("#contents div:first").height();
   $("footer").css("margin-top", contentsBoxHeight * times);
 }
 
+function createCardElement(data) {
+  return `
+  <div class="contents-box">
+    <div class="contents-flame front">
+      <div class="icon">
+        <i class="fa fa-solid ${data.icon}"></i>
+      </div>
+    <div>
+      <p class="small-content-topic">${data.frontTitle}</p>
+    </div>
+  </div>
+
+  <div class="contents-flame back">
+    <p class="small-content-title">${data.backTitle}</p>
+    <p class="content-detail">
+      ${data.content}
+    </p>
+  </div>
+`;
+}
+
 $(function () {
+  cardData.map(function (data) {
+    $("#contents").append(createCardElement(data));
+  });
   const windowSize = $(window).width();
   if (windowSize > 1000) {
     addMarginTopToFooter(3.3);
@@ -31,4 +49,7 @@ $(function () {
   } else {
     addMarginTopToFooter(8.5);
   }
+  $(".contents-box").on("click", function () {
+    $(this).toggleClass("flipped");
+  });
 });
